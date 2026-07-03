ea7d8e26b4b26e2c5ef335d0c3b0c13a5e17e591
Dae Hyun Choi <dhchoikorea@gmail.com>
2026-07-03 13:26:26 +0900

    STEP 2-2 : Footer, Main Page and Javascript

---
+++ includes/footer.php
@@ -0,0 +1,75 @@
+<footer>
+
+<div class="container">
+
+<div class="row">
+
+<div class="col-lg-4">
+
+<h5>SEOJIN MARITIME CO., LTD.</h5>
+
+<p>
+Your Reliable Global Logistics & Trading Partner
+</p>
+
+</div>
+
+<div class="col-lg-4">
+
+<h5>Contact</h5>
+
+<p>
+
+President Officetel #1108<br>
+
+13, Jungang-daero 180beon-gil,<br>
+
+Dong-gu, Busan 48821, Korea
+
+</p>
+
+<p>
+
+TEL : +82-51-246-4835<br>
+
+FAX : +82-51-797-8401
+
+</p>
+
+</div>
+
+<div class="col-lg-4">
+
+<h5>E-Mail</h5>
+
+<p>
+
+dhchoi@seojinmaritime.com<br>
+
+dhchoi@seojinmaritime.co.kr
+
+</p>
+
+</div>
+
+</div>
+
+<hr>
+
+<div class="text-center">
+
+© <?=currentYear()?> SEOJIN MARITIME CO., LTD.
+All Rights Reserved.
+
+</div>
+
+</div>
+
+</footer>
+
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
+
+<script src="assets/js/main.js"></script>
+
+</body>
+</html>
--- index.php
+++ index.php
@@ -1,7 +1,6 @@
 <?php
 
 require_once "includes/config.php";
-require_once "includes/db.php";
 require_once "includes/functions.php";
 
 include "includes/header.php";
@@ -12,15 +11,251 @@ include "includes/header.php";
 
 <div class="container">
 
-<h1>SEOJIN MARITIME</h1>
+<div class="row">
 
-<p>Your Reliable Global Logistics & Trading Partner</p>
+<div class="col-lg-7">
 
-<a href="company.php" class="btn btn-primary">
+<h1>
 
-Company
+SEOJIN MARITIME
+
+</h1>
+
+<p>
+
+Your Reliable Global Logistics & Trading Partner
+
+</p>
+
+<a href="company.php" class="btn btn-main">
+
+About Us
 
 </a>
+
+<a href="contact.php" class="btn btn-light ms-2">
+
+Contact
+
+</a>
+
+</div>
+
+</div>
+
+</div>
+
+</section>
+
+<section class="section">
+
+<div class="container">
+
+<div class="text-center mb-5">
+
+<h2 class="section-title">
+
+WHO WE ARE
+
+</h2>
+
+<p>
+
+SEOJIN MARITIME CO., LTD. is a Korean company specializing in international freight forwarding, shipping agency services and global trading.
+
+</p>
+
+</div>
+
+<div class="row">
+
+<div class="col-lg-4">
+
+<h4>
+
+International Freight Forwarding
+
+</h4>
+
+<p>
+
+Sea Freight
+
+Air Freight
+
+Customs Clearance
+
+Warehousing
+
+Door to Door
+
+Project Cargo
+
+</p>
+
+</div>
+
+<div class="col-lg-4">
+
+<h4>
+
+Shipping Agency
+
+</h4>
+
+<p>
+
+Port Agency
+
+Owner's Protective Agency
+
+Crew Change
+
+Ship Supply
+
+Vessel Coordination
+
+Documentation
+
+</p>
+
+</div>
+
+<div class="col-lg-4">
+
+<h4>
+
+Global Trading
+
+</h4>
+
+<p>
+
+Frozen Squid
+
+Chicken Products
+
+Salmon
+
+Anchovy
+
+Rare Earth
+
+Metal Silicon
+
+Aluminum Products
+
+</p>
+
+</div>
+
+</div>
+
+</div>
+
+</section>
+
+<section class="section bg-light">
+
+<div class="container">
+
+<div class="row">
+
+<div class="col-lg-3 text-center">
+
+<h2>
+
+2011
+
+</h2>
+
+<p>
+
+Established
+
+</p>
+
+</div>
+
+<div class="col-lg-3 text-center">
+
+<h2>
+
+100+
+
+</h2>
+
+<p>
+
+Global Partners
+
+</p>
+
+</div>
+
+<div class="col-lg-3 text-center">
+
+<h2>
+
+6
+
+</h2>
+
+<p>
+
+Continents
+
+</p>
+
+</div>
+
+<div class="col-lg-3 text-center">
+
+<h2>
+
+24/7
+
+</h2>
+
+<p>
+
+Customer Service
+
+</p>
+
+</div>
+
+</div>
+
+</div>
+
+</section>
+
+<section class="section">
+
+<div class="container">
+
+<div class="text-center">
+
+<h2 class="section-title">
+
+GLOBAL NETWORK
+
+</h2>
+
+<p>
+
+Reliable logistics and trading partners throughout Asia, Europe, North America, South America and Oceania.
+
+</p>
+
+<a href="network.php" class="btn btn-main">
+
+View Network
+
+</a>
+
+</div>
 
 </div>
 
