0b822fc4bb3d5fce01252827f2830f34bc9043fe
Dae Hyun Choi <dhchoikorea@gmail.com>
2026-07-03 13:04:42 +0900

    STEP 2-1 : Header, Navbar and CSS

---
+++ includes/navbar.php
@@ -0,0 +1,125 @@
+<nav class="navbar navbar-expand-lg navbar-dark fixed-top">
+
+<div class="container">
+
+<a class="navbar-brand" href="index.php">
+
+<img src="assets/images/logo/logo.png" height="55">
+
+</a>
+
+<button class="navbar-toggler" type="button"
+data-bs-toggle="collapse"
+data-bs-target="#mainmenu">
+
+<span class="navbar-toggler-icon"></span>
+
+</button>
+
+<div class="collapse navbar-collapse" id="mainmenu">
+
+<ul class="navbar-nav ms-auto">
+
+<li class="nav-item">
+
+<a class="nav-link" href="index.php">
+
+HOME
+
+</a>
+
+</li>
+
+<li class="nav-item">
+
+<a class="nav-link" href="company.php">
+
+COMPANY
+
+</a>
+
+</li>
+
+<li class="nav-item">
+
+<a class="nav-link" href="business.php">
+
+BUSINESS
+
+</a>
+
+</li>
+
+<li class="nav-item">
+
+<a class="nav-link" href="products.php">
+
+PRODUCTS
+
+</a>
+
+</li>
+
+<li class="nav-item">
+
+<a class="nav-link" href="network.php">
+
+NETWORK
+
+</a>
+
+</li>
+
+<li class="nav-item">
+
+<a class="nav-link" href="news.php">
+
+NEWS
+
+</a>
+
+</li>
+
+<li class="nav-item">
+
+<a class="nav-link" href="certificates.php">
+
+CERTIFICATES
+
+</a>
+
+</li>
+
+<li class="nav-item">
+
+<a class="nav-link" href="contact.php">
+
+CONTACT
+
+</a>
+
+</li>
+
+<li class="nav-item ms-3">
+
+<a href="index.php?lang=ko">
+
+🇰🇷
+
+</a>
+
+<a href="index.php?lang=en">
+
+🇺🇸
+
+</a>
+
+</li>
+
+</ul>
+
+</div>
+
+</div>
+
+</nav>
